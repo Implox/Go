c2ddf302f61f97ad3165ae6b9ed958f39a84aef1
Alex Flores <alexmflores@gmail.com>
2012-04-04 18:42:08 -0300

    Update Board.fs

--- Board.fs
+++ Board.fs
@@ -1,14 +1,12 @@
-﻿module Board
+module Board
 
 /// Tells whether a given space on the board is empty, occupied by a black piece, or occupied by a white piece.
-/// Black is given the first "occupied" state because it is the color which moves first at the beginning of a game.
 type CellState =
 |Empty = 0
 |Black = 1
 |White = 2
 
 /// Tells whether or not a given space on the board has been visited when searching for its liberties.
-/// Used because determining the liberties of a given cell or group of cells requires a depth-first search algorithm.
 type VisitState =
 |Visited
 |NotVisited
@@ -16,40 +14,49 @@ type VisitState =
 type ActiveBoard (sideLength : int) =
     let CellGrid = Array2D.create sideLength sideLength CellState.Empty
 
-    /// Array of cells visited by the GetLiberties function
-    /// Must be reset each time GetLiberties has been evaluated
+    /// Array of cells visited by the GetLiberties function.
+    /// Must be reset each time GetLiberties has been evaluated.
     let VisitGrid = Array2D.create sideLength sideLength VisitState.NotVisited
 
     /// List of all the adjacent cells to a given stone.
-    let Neighbors (x : int, y : int) = [(x - 1, y); (x + 1, y); (x, y - 1); (x, y + 1)] |> List.filter (fun (x, y) -> x >= 0 && x < sideLength && y >= 0 && y < sideLength)
+    let Neighbors (x : int, y : int) = 
+        [(x - 1, y); (x + 1, y); (x, y - 1); (x, y + 1)] 
+        |> List.filter (fun (x, y) -> x >= 0 && x < sideLength && y >= 0 && y < sideLength)
 
-    /// Gets the CellState of a given cell
+    /// Gets the CellState of a given cell.
     let GetCellState (x : int, y : int) = CellGrid.[x, y]
 
-    /// Gets the liberties of a cell or group of cells
+    /// Gets the liberties of a cell or group of cells.
     let rec GetLiberties ((x : int, y : int)) =
-        if GetCellState (x, y) <> CellState.Empty then
+        if GetCellState (x, y) = CellState.Empty then []
+        else
             VisitGrid.[x, y] <- VisitState.Visited
             let currentState = GetCellState (x, y)
             let GetVisitState (x : int, y : int) = VisitGrid.[x, y]
-            let neighborList =
+
+            /// List of cells adjacent to the current one. 
+            /// Only cells which are empty or occupied by the same color stone are listed.
+            let neighborList = 
                 (x, y) 
                 |> Neighbors 
                 |> List.filter (fun n -> GetCellState n = CellState.Empty || GetCellState n = currentState)
+
+            /// List of liberties (empty cells) available to the current cell's stone.
             let currentLiberties = neighborList |> List.filter (fun n -> GetCellState n = CellState.Empty)
-            let currentNeighbors = neighborList |> List.filter (fun n -> GetCellState n <> CellState.Empty && GetVisitState n <> VisitState.Visited)
+
+            /// Trimmed version of neighborList that only contains non-empty cells which have not been visited by GetLiberties.
+            let currentNeighbors = neighborList |> List.filter (fun n -> GetVisitState n <> VisitState.Visited)
             match currentNeighbors with
             | [] -> currentLiberties
             | [_] -> currentLiberties @ GetLiberties currentNeighbors.[0]
             | _ -> currentLiberties @ List.collect GetLiberties currentNeighbors
-        else []
 
     /// Method called when a player places one of their stones in a given cell of the board.
     member this.PlaceStone (x : int, y : int, state : CellState) =
         if CellGrid.[x, y] = CellState.Empty then CellGrid.[x, y] <- state
         else failwith "Cell already occupied"
 
-    /// Returns the liberties of stone or group of stones
+    /// Returns the liberties of stone or group of stones.
     member this.Liberties (x : int, y : int) = GetLiberties (x, y)
         
 
